b592e2e664a79765dd734423f82fe7ea77fd007f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 09:28:23 +0000

    Deploy web-front-end/react-typescript-todo to github.com/web-front-end/react-typescript-todo.git:gh-pages

---
+++ precache-manifest.f86d467bf8e327771549ff41d2dbc6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c333d1781f56f4ff668a",
+    "url": "/static/js/main.357e205d.chunk.js"
+  },
+  {
+    "revision": "a75fc9ecd6f05bf091e4",
+    "url": "/static/js/2.54900857.chunk.js"
+  },
+  {
+    "revision": "c333d1781f56f4ff668a",
+    "url": "/static/css/main.2b4d3836.chunk.css"
+  },
+  {
+    "revision": "a75fc9ecd6f05bf091e4",
+    "url": "/static/css/2.bf6e6662.chunk.css"
+  },
+  {
+    "revision": "bb1982a31346b76aa544745ad77cb2f9",
+    "url": "/index.html"
+  }
+];
