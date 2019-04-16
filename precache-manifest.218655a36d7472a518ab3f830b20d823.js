45a92c845e5eb673eda9a51ad166f4a35129f763
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 09:32:38 +0000

    Deploy web-front-end/react-typescript-todo to github.com/web-front-end/react-typescript-todo.git:gh-pages

---
+++ precache-manifest.218655a36d7472a518ab3f830b20d823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c2f259fe6c094494a4",
+    "url": "/react-typescript-todo/static/js/runtime~main.b87fe60e.js"
+  },
+  {
+    "revision": "82c572ca482c6a387846",
+    "url": "/react-typescript-todo/static/js/main.dab24631.chunk.js"
+  },
+  {
+    "revision": "a75fc9ecd6f05bf091e4",
+    "url": "/react-typescript-todo/static/js/2.54900857.chunk.js"
+  },
+  {
+    "revision": "82c572ca482c6a387846",
+    "url": "/react-typescript-todo/static/css/main.2b4d3836.chunk.css"
+  },
+  {
+    "revision": "a75fc9ecd6f05bf091e4",
+    "url": "/react-typescript-todo/static/css/2.bf6e6662.chunk.css"
+  },
+  {
+    "revision": "c9b1407d4d69abe738008c3f16193a9d",
+    "url": "/react-typescript-todo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f86d467bf8e327771549ff41d2dbc6f7.js"
+  "/react-typescript-todo/precache-manifest.218655a36d7472a518ab3f830b20d823.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-typescript-todo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
